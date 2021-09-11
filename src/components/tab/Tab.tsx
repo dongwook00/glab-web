341ebbd83c8ae8dbdbcb30eca06754ac0deeab52
Dongwook Kim <dongwook224@gmail.com>
2021-09-11 15:07:03 +0900

    add component: tab

--- src/components/tab/Tab.tsx
+++ src/components/tab/Tab.tsx
@@ -1,14 +1,17 @@
 import styles from './Tab.module.scss';
+import { ReactComponent as FilterIcon } from '../../assets/images/filter.svg';
 
 const Tab: React.FC = () => {
   return (
     <header className={styles.tab}>
       <div className={styles.tabButtonGroup}>
-        <button>glam</button>
+        <button className={styles.active}>glam</button>
         <button>근처</button>
         <button>라이브</button>
       </div>
-      <div className={styles.filter}>filter</div>
+      <div className={styles.filter}>
+        <FilterIcon />
+      </div>
     </header>
   );
 };
