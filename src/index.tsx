e34d89a5429d00f2ca402bf25756f23cc5f06c02
Dongwook Kim <dongwook224@gmail.com>
2021-09-12 23:20:31 +0900

    add feature: scroll snap

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,16 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.scss';
 
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistStore(store)}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </PersistGate>
   </Provider>,
   document.getElementById('root'),
