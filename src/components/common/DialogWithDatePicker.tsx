cbbb59759fb956d6e161faf038cab1dd7c903567
Dongwook Kim <dongwook224@gmail.com>
2021-09-12 21:11:08 +0900

    add feature: birthday update

--- src/components/common/DialogWithDatePicker.tsx
+++ src/components/common/DialogWithDatePicker.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -5,6 +6,14 @@ import DialogActions from '@material-ui/core/DialogActions';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
+import { useAppDispatch } from '../../redux/hooks';
+import { update } from '../../redux/profileSlice';
+
+interface DialogWithDatePickerProps {
+  open: boolean;
+  value: string;
+  onClose: () => void;
+}
 
 const useStyles = makeStyles({
   dialogTitleRoot: {
@@ -15,19 +24,37 @@ const useStyles = makeStyles({
   },
 });
 
-const DialogWithDatePicker: React.FC = () => {
+const DialogWithDatePicker: React.FC<DialogWithDatePickerProps> = (props) => {
   const classes = useStyles();
+  const dispatch = useAppDispatch();
+  const [value, setValue] = useState<string>();
+
+  useEffect(() => {
+    setValue(props.value);
+  }, [props.value, props.open]);
+
+  const onButtonClick = () => {
+    dispatch(update({ type: 'birthday', value }));
+    props.onClose();
+  };
+
   return (
     <div>
-      <Dialog open={false} aria-labelledby="form-dialog-title" maxWidth="xs" fullWidth>
+      <Dialog open={props.open} aria-labelledby="form-dialog-title" maxWidth="xs" fullWidth>
         <DialogTitle classes={{ root: classes.dialogTitleRoot }} id="form-dialog-title">
           <b>생일을 변경하시겠어요?</b>
         </DialogTitle>
         <DialogContent>
-          <TextField id="date" type="date" defaultValue="2017-05-24" fullWidth />
+          <TextField
+            id="date"
+            type="date"
+            fullWidth
+            value={value}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)}
+          />
         </DialogContent>
         <DialogActions classes={{ root: classes.dialogActionRoot }}>
-          <Button fullWidth variant="contained" color="primary">
+          <Button fullWidth variant="contained" color="primary" onClick={onButtonClick}>
             생일 변경
           </Button>
         </DialogActions>
--- src/components/profile/Primary.tsx
+++ src/components/profile/Primary.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import { useAppSelector } from '../../redux/hooks';
 import { LayoutProfileSection, LayoutFormGroup } from '../layout';
-import { DialogWithTextField } from '../common';
+import { DialogWithTextField, DialogWithDatePicker } from '../common';
 import styles from './Primary.module.scss';
 
 const Primary: React.FC = () => {
   const profile = useAppSelector((state) => state.profile);
   const [nicknameEdit, setNicknameEdit] = useState<boolean>(false);
+  const [showBirthdayDialog, setShowBirthdayDialog] = useState<boolean>(false);
 
   return (
     <>
@@ -17,11 +18,16 @@ const Primary: React.FC = () => {
           </div>
         </LayoutFormGroup>
         <LayoutFormGroup type="성별">남성</LayoutFormGroup>
-        <LayoutFormGroup type="생일">1985-11-10</LayoutFormGroup>
+        <LayoutFormGroup type="생일">
+          <div className={styles.nickname} onClick={() => setShowBirthdayDialog(true)}>
+            {profile.birthday}
+          </div>
+        </LayoutFormGroup>
         <LayoutFormGroup type="위치">서울 강남구</LayoutFormGroup>
       </LayoutProfileSection>
 
       <DialogWithTextField value={profile.nickname} open={nicknameEdit} onClose={() => setNicknameEdit(false)} />
+      <DialogWithDatePicker value={profile.birthday} open={showBirthdayDialog} onClose={() => setShowBirthdayDialog(false)} />
     </>
   );
 };
